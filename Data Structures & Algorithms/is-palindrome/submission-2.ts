c6cdae6446d77b9d153364b808e1d858f7be9121
Vandervitt <Vandervitt.li@gmail.com>
2026-07-07 18:05:05 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        // Clean s to lowercase
+        const cleanStr = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+        // Initial two pointer
+        let left = 0;
+        let right = cleanStr.length - 1;
+
+        // 
+        while (left < right) {
+            const leftStr = cleanStr[left];
+            const rightStr = cleanStr[right];
+
+            if (leftStr !== rightStr) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
